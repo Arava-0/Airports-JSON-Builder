d3121591ac0ce42f32d49fbb4c4dae657b30356f
dkeenan <keenan.deboissy@epitech.eu>
2025-05-12 14:28:59 +0200

    [UPDATE] Build script to include ARM Linux version

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
-deno compile --allow-net --allow-write --target x86_64-pc-windows-msvc main.ts
-deno compile --allow-net --allow-write --target x86_64-unknown-linux-gnu main.ts
+deno compile --allow-net --allow-write --output Airports-JSON-Windows-x86_64.exe --target x86_64-pc-windows-msvc main.ts
+deno compile --allow-net --allow-write --output Airports-JSON-Linux-x86_64 --target x86_64-unknown-linux-gnu main.ts
+deno compile --allow-net --allow-write --output Airports-JSON-Linux-ARM64 --target aarch64-unknown-linux-gnu main.ts
